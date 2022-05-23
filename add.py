a01c144ff5328cf458ce6ab6eb6330590d355334
Evgeniy112 <evgeniy.pogorelov@bk.ru>
2022-05-24 00:39:50 +0300

    my first bot

--- add.py
+++ add.py
@@ -21,21 +21,21 @@ def values(message: telebot.types.Message):
 
 @bot.message_handler(content_types=['text', ])
 def convert(message: telebot.types.Message):
-    values = message.text.split(' ')
+    try:
+        values = message.text.split(' ')
 
-    if len(values) != 3:
-        raise ConvertionException('Неправильно введены данные')
+        if len(values) != 3:
+            raise ConvertionException('Неправильно введены данные')
 
-    quote, base, amount = values
-    total_base = CryptoConverter.convert(quote, base, amount)
-
-    text = f'Цена {amount} {quote} в {base} - {total_base}'
-    bot.send_message(message.chat.id, text)
-
-
-
-
-
+        quote, base, amount = values
+        total_base = CryptoConverter.convert(quote, base, amount)
+    except ConvertionException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду \n{e}')
+    else:
+        text = f'Цена {amount} {quote} в {base} - {float(total_base)*float(amount)}'
+        bot.send_message(message.chat.id, text)
 
 
 bot.polling()
